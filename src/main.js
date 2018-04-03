d43f9733ff4b80f42fc8af2fc8a7688fb93f240e
AleksandrGunko <alex.gunko.work@gmail.com>
2018-04-03 23:20:47 +0300

    Added projects files

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './main.scss';
+import './js/jquery.js';
+import './js/bootstrap.min.js';
+var ProgressBar = require('./js/progressbar.js');
+//import './js/slick.min.js';
+console.log('ok');
